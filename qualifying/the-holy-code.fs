82d4a45bff9b298e24ebfcf5eb979943c070a95c
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2020-02-20 21:06:57 +0200

    Add our first working solution!! 🎉🐱‍💻

--- qualifying/the-holy-code.fs
+++ qualifying/the-holy-code.fs
@@ -1,12 +1,14 @@
 module HashCode2020
 
 open System
+open System.Collections.Generic
 open System.Diagnostics
 open System.IO
 
 type Book = int
 
 type Library = {
+    Id: int
     Books: Book Set
     SignupTime: int
     ScanningRate: int
@@ -17,6 +19,8 @@ type ProblemStatement = {
     BookScores: int[]
     Deadline: int
 }
+with
+    member x.GetBookScore (book: Book) = x.BookScores.[book]
 
 let parseInput lines =
     let parseLine (x: string) = x.Split(' ') |> Array.map int
@@ -26,9 +30,10 @@ let parseInput lines =
     let libraries =
         xs
         |> List.chunkBySize 2
-        |> List.map (fun [libInfo; books] -> 
+        |> List.mapi (fun i [libInfo; books] ->
             let [|_; signupTime; scanningRate|] = parseLine libInfo
             {
+                Id = i
                 Books = set <| parseLine books
                 SignupTime = signupTime
                 ScanningRate = scanningRate
@@ -39,6 +44,39 @@ let parseInput lines =
         Deadline = deadline
     }
 
+type ProblemSubmission = ProblemSubmission of (int * Book list) list
+
+let solveIt statement () =
+    let libraries =
+        statement.Libraries
+        |> List.sortByDescending (fun lib ->
+            let totalScore = Seq.sumBy statement.GetBookScore lib.Books |> float
+            Math.Cbrt(float lib.ScanningRate * totalScore / float lib.SignupTime))
+
+    let scannedBooks = HashSet()
+
+    libraries
+    |> List.map (fun lib ->
+        let books =
+            lib.Books
+            |> Seq.where (scannedBooks.Contains >> not)
+            |> Seq.sortByDescending statement.GetBookScore
+            |> List.ofSeq
+        Seq.iter (scannedBooks.Add >> ignore) books
+        lib.Id, books)
+    |> ProblemSubmission
+
+let renderSubmission (ProblemSubmission sub) =
+    sub
+    |> List.collect (fun (id, books) ->
+        [
+            sprintf "%d %d" id books.Length
+            books |> Seq.map string |> String.concat " "
+        ]
+    )
+    |> (fun xs -> string sub.Length :: xs)
+    |> String.concat "\n"
+
 let timeIt label f =
     let sw = Stopwatch()
     sw.Start()
@@ -46,8 +84,6 @@ let timeIt label f =
     sw.Stop()
     printfn "Time for %s: %O" label sw.Elapsed
     x
-
-let solveIt statement () = [| |]
 
 Environment.CurrentDirectory
 |> Directory.EnumerateFiles
@@ -57,5 +93,5 @@ Environment.CurrentDirectory
     let data = File.ReadLines x |> List.ofSeq
     let outputFileName = Path.ChangeExtension(x, ".out")
     let problemStatement = parseInput data
-    let theHolySolution = solveIt problemStatement |> timeIt x
-    File.WriteAllLines(outputFileName, theHolySolution))
+    let theHolySolution = solveIt problemStatement |> timeIt x |> renderSubmission
+    File.WriteAllText(outputFileName, theHolySolution))
